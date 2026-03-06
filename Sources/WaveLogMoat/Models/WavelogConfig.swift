ed83c66a4d0f6b6d3779db555e45ba7a3b08f1fb
DL5MN <dl5mn@users.noreply.github.com>
2026-03-06 08:00:32 +0100

    feat: add Wavelog configuration and station profile models

---
+++ Sources/WaveLogMoat/Models/WavelogConfig.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public struct WavelogConfig: Codable, Sendable, Equatable {
+    public var wavelogURL: String = ""
+    public var stationProfileID: String = ""
+
+    public var textUDPPort: UInt16 = 2333
+    public var enableTextUDP: Bool = true
+
+    public var binaryUDPPort: UInt16 = 2237
+    public var enableBinaryUDP: Bool = false
+
+    public var listenAddress: String = "127.0.0.1"
+
+    public var showInDock: Bool = false
+    public var showInMenuBar: Bool = true
+    public var launchAtLogin: Bool = false
+    public var showNotifications: Bool = true
+    public var allowSelfSignedCerts: Bool = true
+    public var httpTimeout: Int = 5000
+
+    public var showFrequencyInMenuBar: Bool = true
+
+    public init() {}
+}
